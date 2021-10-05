ed43a4457d6b4f21384814037fbf15c10c2f243a
Vanshika Bhasin <vanshikabhasin9@gmail.com>
2021-10-05 22:32:02 +0530

    added review section

---
+++ src/components/Reviews.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "../css/Reviews.css";
+
+
+function Reviews(props) {
+  return (
+    <div className="reviews">
+      <div>
+        <h3>What our customers say...</h3>
+      </div>
+    <div id="carouselExampleIndicators" className="carousel slide reviewscontainer" data-ride="carousel">
+  <ol className="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <div className="cc">
+<div className="reviewsimg">
+<img src="images/RabwikInnovations.jpg" className="d-block imager w-100" alt="..."/>
+<h6 className="imgname">Name here</h6>
+</div>
+    <div className="reviewstext">
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum....</p>
+    </div>
+    </div></div>
+    <div className="carousel-item">
+      <div className="cc">
+<div className="reviewsimg">
+<img src="images/RabwikInnovations.jpg" className="d-block imager w-100" alt="..."/>
+<h6 className="imgname">Name here</h6>
+</div>
+    <div className="reviewstext">
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum....</p>
+    </div>
+    </div>
+    </div>
+    <div className="carousel-item">
+      <div className="cc">
+<div className="reviewsimg">
+<img src="images/RabwikInnovations.jpg" className="d-block imager w-100" alt="..."/>
+<h6 className="imgname">Name here</h6>
+</div>
+    <div className="reviewstext">
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum
+          Nulla vitae elit libero, a pharetra augue mollis interdum....</p>
+    </div>
+    </div>
+  </div></div>
+  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
+</div>
+      );
+      }
+export default Reviews;
---
+++ src/css/ScrolltotopStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+  
+export const Content = styled.div`
+   overflowY: scroll;
+   height: 2500px;
+`;
+  
+export const Button = styled.div`
+   position: fixed; 
+   width: 100%;
+   left: 92%;
+   bottom: 40px;
+   height: 20px;
+   font-size: 2.4rem;
+   z-index: 1;
+   cursor: pointer;
+   color: blue`;
