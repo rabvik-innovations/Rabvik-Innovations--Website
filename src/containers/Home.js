460dc169f4ea99cb4a252f359bd75d5d234380a6
Vanshika Bhasin <vanshikabhasin9@gmail.com>
2021-10-12 23:09:01 +0530

    added about section

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,7 @@ import GalleryCard from "../components/GalleryCard";
 import PhotoGallery from "../components/PhotoGallery";
 import Hero from '../components/Hero';
 
-import About from '../components/About';
+import About from '../containers/About';
 import AdvisorCard from '../components/AdvisorCard';
 import WhatWeDo from '../components/Whatwedo';
 import Navbar from '../components/Navbar';
@@ -29,15 +29,43 @@ function Home() {
 <section>
 <Hero/>
 </section>
-
+<section>
+<div className="about">
+      <div className="section-title text-center">
+        <h2>About Rabvik®</h2>
+      </div>
+      <div className="row">
+        <div className="aboutstyle">
+        <div className="col">
+        <video controls="autoplay muted"
+          src={"https://rabvik.com/images/Rabvik-Teaser.mp4"}
+        />
+        </div>
+        <div className="col2">
+          <p>
+          Incredible breakthroughs and spectacular advancements in 
+          technology are changing the world as we know it by leaps and bounds. If we are to keep up with this frenetic pace of technological change in industry and education, technology minded and 
+          design thinking are essential at the very earliest steps, especially for our future generations.
+          </p>
+          <p>
+          Rabvik® Innovations is just the kind of unique platform we 
+          need today to encourage breakthrough technologies and Innovation’s world class mentoring program creates the optimum biosphere to incubate ideas and innovations right from the school level. One of the exemplary institutes for innovation, 
+          entrepreneurship and robotics training for school children,
+          </p><br/>
+         <div className="aboutbtn">
+         <Link to="/aboutus">
+           <button>More about us</button>
+           </Link>
+           </div>
+        </div></div>
+      </div>
+    </div>
+      </section>
 
 <section>
   <WhatWeDo/>
   </section>
 
-      <section>
-        <About />
-      </section>
 
       {/* {Gallery Section} */}
       <section id="gallery-section" className="gallery-item">
