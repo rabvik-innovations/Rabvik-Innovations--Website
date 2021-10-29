bc705b1ce4fa9745fe927e5c71cea78881b85ad5
vijetasaini0211 <vijetasaini0211@gmail.com>
2021-10-29 19:51:16 +0530

    add styling to what we do section

--- src/components/Whatwedo.js
+++ src/components/Whatwedo.js
@@ -11,35 +11,42 @@ function WhatWeDo(){
             <p class="service-main-heading">Features</p>
           </div>
         </div>
+        
         <div class="row">
-          <div class="col-md-6 col-lg-6 col-12">
-            <div class="icon-box">
+          <div class="center-container col-md-6 col-lg-6 col-12">
+
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-12" id="icon-box-left">
+            <div class="icon-box" >
               <i class="fa fa-briefcase service-icon"></i>
               <p class="service-title"><a href="#">Lorem Ipsum</a></p>
               <p class="service-para">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
             </div>
-          </div>
-          <div class="col-md-6 col-lg-6 col-12 mt-4 mt-md-0">
-            <div class="icon-box">
-              <i class="fa fa-clipboard service-icon"></i>
+            <div class="icon-box icon-box-even" >
+              <i class="fa fa-clipboard service-icon "></i>
               <p class="service-title"><a href="#">Dolor Sitema</a></p>
               <p class="service-para">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
             </div>
           </div>
-          <div class="col-md-6 col-lg-6 col-12 mt-4 mt-md-0">
+
+          
+
+          <div class="col-md-3 col-lg-3 col-12 mt-4 mt-md-0" id="icon-box-right">
+            <div class="icon-box icon-box-even">
+              <i class="fa fa-clipboard service-icon "></i>
+              <p class="service-title"><a href="#">Dolor Sitema</a></p>
+              <p class="service-para">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            </div>
+
             <div class="icon-box">
-              <i class="fa fa-chart-bar service-icon"></i>
+              <i class="fa fa-chart-bar service-icon" ></i>
               <p class="service-title"><a href="#">Sed ut perspiciatis</a></p>
               <p class="service-para">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
             </div>
           </div>
-          <div class="col-md-6 col-lg-6 col-12 mt-4 mt-md-0">
-            <div class="icon-box">
-              <i class="fa fa-binoculars service-icon"></i>
-              <p class="service-title"><a href="#">Nemo Enim</a></p>
-              <p class="service-para">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-          </div>
+          
+         
           {/* <div class="col-md-6 col-lg-6 col-12 mt-4 mt-md-0">
             <div class="icon-box">
               <i class="fa fa-cog service-icon"></i>
