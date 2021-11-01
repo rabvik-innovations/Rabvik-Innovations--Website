437964f4a418a685f8e12f88f16ed5cface3d3f5
Uma Negi <umanegi267@gmail.com>
2021-11-01 18:18:38 +0530

    Update periodical section

--- src/components/PeriodicalCard.js
+++ src/components/PeriodicalCard.js
@@ -1,7 +1,9 @@
 
 function PeriodicalCard(props) {
     return (
-        <div className="col-lg-4 col-md-6 align-items-stretch text-center">
+        <div className="col-lg-4 col-md-6 align-items-stretch text-center periodical-card">
+
+            <h3> {props.text} </h3>
         
             <a href={props.link} target="_blank">
                 <img className="periodical-img" src={props.image}  alt={props.imgAlt} />
