742b56b918f41c1b7905e10a08cb1b9d7aad043a
Uma Negi <umanegi267@gmail.com>
2021-11-06 11:01:47 +0530

    update innovation lab, minor changes(footer)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,49 +6,49 @@ function Navbar() {
     return (
         <div className="container" id="header">
 
-            <nav class="navbar fixed-top navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="/">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
                         {/* Navbar */}
                         <img src="../images/Rabvik Innovations.png" id="logo" style={{height:"70px", width:"129px", marginTop:"6%", marginBottom:"2%"}} />
                     </Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav ms-auto nav-menu">
-                            <li class="nav-item">
-                                <Link class="nav-link active" to="/">Home</Link>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto nav-menu">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/aboutus">About</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/aboutus">About</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/gallery">Gallery</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/gallery">Gallery</Link>
                             </li>
-                            {/* <li class="nav-item">
-                                <Link class="nav-link" to="/blog">Blogs</Link>
+                            {/* <li className="nav-item">
+                                <Link className="nav-link" to="/blog">Blogs</Link>
                             </li> */}
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/ourpartners">Partners</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/ourpartners">Partners</Link>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     School Partnership Program
                                 </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><Link class="dropdown-item" to="/innovation-lab">Innovation Lab</Link></li>
-                                    <li><Link class="dropdown-item" to="/tinker-times">Tinker Times</Link></li>
+                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><Link className="dropdown-item" to="/innovation-lab">Innovation Lab</Link></li>
+                                    <li><Link className="dropdown-item" to="/tinker-times">Tinker Times</Link></li>
 
                                 </ul>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link " to="/team">
+                            <li className="nav-item">
+                                <Link className="nav-link " to="/team">
                                     Team
                                 </Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link " to="/contact">Contact</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link " to="/contact">Contact</Link>
                             </li>
                         </ul>
                     </div>
