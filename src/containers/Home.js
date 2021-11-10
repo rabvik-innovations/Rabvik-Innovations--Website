6c939c81bde431181bea04213397ed357e52b205
Uma Negi <umanegi267@gmail.com>
2021-11-10 18:30:03 +0530

    changes (responsive)

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -167,7 +167,7 @@ Encourage breakthrough technologies and Innovations of your children right from 
             <h2 style={{color:"white"}}>Our Advisors</h2>
           </div>
 
-          <div className="row advisor-card-row">
+          <div className="row advisor-card-row justify-content-center">
             <AdvisorCard
               name="Anupam Sen"
               linkedin="https://www.linkedin.com/in/anupam-sen-a16b7013/?originalSubdomain=in"
