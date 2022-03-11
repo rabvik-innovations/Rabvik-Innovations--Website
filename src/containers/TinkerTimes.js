6b21288fb426556839bfa0272bee91b7a940dd31
rabvik-innovations <89805241+rabvik-innovations@users.noreply.github.com>
2022-03-12 00:59:21 +0530

    Update TinkerTimes.js

--- src/containers/TinkerTimes.js
+++ src/containers/TinkerTimes.js
@@ -21,79 +21,101 @@ function TinkerTimes(props) {
 
 
                     <div className="row">
+                        
+                        <TinkerTimesCard
+                            text="February-2022"
+                            image="images/tinker-times-feb-22.jpg"
+                            imgAlt="tinker-times-feb-22"
+                            link="documents/Tinker Times - Feb 2022.pdf"
+                        />                           
+                        
+                        
+                        <TinkerTimesCard
+                            text="January-2022"
+                            image="images/tinker-times-jan-22"
+                            imgAlt="tinker-times-jan-22"
+                            link="documents/Tinker Times Jan 2022.pdf"
+                        />                        
+                        
+                        <TinkerTimesCard
+                            text="December-2021"
+                            image="images/tinker-times-dec-21.jpg"
+                            imgAlt="tinker-times-dec-21"
+                            link="documents/Tinker Times- December.pdf"
+                        />
 
                         <TinkerTimesCard
-                            text="November"
+                            text="November-2021"
                             image="images/tinker-times-november.jpg"
                             imgAlt="tinker-times-november"
                             link="documents/Tinker Times Nov.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="October"
+                            text="October-2021"
                             image="images/tinker-times-october.jpg"
                             imgAlt="tinker-times-october"
                             link="documents/October.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="September"
+                            text="September-2021"
                             image="images/tinker-times-september.jpg"
                             imgAlt="tinker-times-september"
                             link="documents/9 September.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="August"
+                            text="August-2021"
                             image="images/tinker-times-august.jpg"
                             imgAlt="tinker-times-august"
                             link="documents/8 August.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="July"
+                            text="July-2021"
                             image="images/tinker-times-july.jpg"
                             imgAlt="tinker-times-july"
                             link="documents/7 July.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="June"
+                            text="June-2021"
                             image="images/tinker-times-june.jpg"
                             imgAlt="tinker-times-june"
                             link="documents/6 June.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="May"
+                            text="May-2021"
                             image="images/tinker-times-may.jpg"
                             imgAlt="tinker-times-may"
                             link="documents/5 May.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="April"
+                            text="April-2021"
                             image="images/tinker-times-april.jpg"
                             imgAlt="tinker-times-april"
                             link="documents/4 April.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="January"
+                            text="January-2021"
                             image="images/tinker-times-jan.jpg"
                             imgAlt="tinker-times-january"
                             link="documents/2 January.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="January V2"
+                            text="January-2021 V2"
                             image="images/tinker-times-jan-V2.jpg"
                             imgAlt="tinker-times-january-V2"
                             link="documents/3 January V2.pdf"
                         />
 
                         <TinkerTimesCard
-                            text="October 2019"
+                            text="October-2019"
                             image="images/tinker-times-oct-19.jpg"
                             imgAlt="tinker-times-october2019"
                             link="documents/1 October 2019.pdf"
